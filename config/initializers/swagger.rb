4253bee24aeea95a2b4393db1d08a952174b4ccf
Artur Hebda <arturhebda@gmail.com>
2014-05-22 14:20:08 +0200

    Use most common port of rails server in swagger initializer.

--- config/initializers/swagger.rb
+++ config/initializers/swagger.rb
@@ -1,6 +1,6 @@
 GrapeSwaggerRails.options.url      = '/api/v1/swagger_doc.json'
 GrapeSwaggerRails.options.app_name = 'Swagger'
-GrapeSwaggerRails.options.app_url  = 'http://localhost:3002'
+GrapeSwaggerRails.options.app_url  = 'http://localhost:3000'
 unless Rails.env.development?
   GrapeSwaggerRails.options.api_key_name = 'token'
   GrapeSwaggerRails.options.api_key_type = 'query'
